070eeb5b5b445e0bb7d0184c4fd3073d3ad8c8fe
unknown <misbahqadri92@gmail.com>
2022-03-07 21:37:11 +0530

    Scarfold completed

---
+++ lib/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    int no = 21;
+    String name = "Misbah";
+    return  Scaffold(
+      appBar: AppBar(
+        title:Text("FLutter App") ,
+      ),
+        body: Center(
+          child: Container(
+            child: Text("Welcome to $name, roll no is $no"),
+          ),
+        ),
+        drawer: Drawer(),
+      );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/home_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
+    int no = 21;
+    String name = "Misbah";
+    double pi = 3.14;
+    bool isFemale = true;
+    // num takes int and double
+    num temp = 30.5;
+    // var takes all data types
+    var day = "Tuesday";
+    // const pi = 3.14;
+    // final
+
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("Welcome to MISBAH"),
-          ),
-        ),
-      ),
+      home:HomePage(),
     );
   }
 }
